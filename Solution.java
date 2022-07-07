097e5d93115a6c800536f62bdad2d865d05d8afa
Neha <nehapriyaekka@gmail.com>
2022-07-07 18:25:16 +0530

    added-2pointer

--- Solution.java
+++ Solution.java
@@ -1,27 +1,25 @@
 import java.util.*;
+
 class Solution {
     public static void main(String[] args) {
-        String ans=""; String key="the quick brown fox jumps over the lazy dog";
-        String message="vkbs bs t suepuv";
-        Map<Character,Character> s = new HashMap<Character,Character>(); char c; char code='a';
-        for(int i=0; i< key.length() && code<='z'; i++){
-            //if(code=='z')break;
-            c = key.charAt(i);
-            if(c==' ')continue;
-            if(s.containsKey(c)==false){
-                s.put(c,code);
+        String s1 = "", s2 = "", s3 = "a";
+
+        int i = 0, j = 0, k = 0;
+        char c3;
+        while (k < s3.length()) {
+            c3 = s3.charAt(k);
+            while (i < s1.length() && k < s3.length() - 1 && c3 == s1.charAt(i)) {
+                c3 = s3.charAt(++k);
+                i++;
             }
-            System.out.println(s.get(c));
-            
-            //System.out.println(s.);
-            code++;
+            while (j < s2.length() && k < s3.length() - 1 && c3 == s2.charAt(j)) {
+                c3 = s3.charAt(++k);
+                j++;
+            }
+            if (c3 != s1.charAt(i) && c3 != s2.charAt(j))
+                System.out.println("false");
         }
-        for(int i=0; i<message.length(); i++){
-            c= message.charAt(i);
-            if(c==' ')ans+=' ';
-            else
-            ans+= s.get(c);
-        }
-        System.out.println(ans);
+
+        System.out.println("true");
     }
 }
